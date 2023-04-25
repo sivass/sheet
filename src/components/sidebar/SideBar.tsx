a971dc17844afd575c43a76fb2acb6a86701dc83
sivass <sivass2703@gmail.com>
2023-04-25 21:55:31 +0800

    Added z-index to header and sidebar layout to overflow main layout content

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -2,7 +2,7 @@ import sheetLogo  from '../../assets/logo/sheet-white.png';
 import Menu from '../menu/Menu';
 function SideBar() {
     return (
-        <div className="w-72 h-full bg-emerald-500 rounded-r-3xl absolute inset-x-0">
+        <div className="w-72 h-full bg-emerald-500 rounded-r-3xl absolute inset-x-0 z-20">
         <div className="grid w-full place-content-center h-20 p-4">
             <div className="container max-w-screen-lg mx-auto p-2">
                 <img src={sheetLogo} alt="sheet" className='w-40' />
